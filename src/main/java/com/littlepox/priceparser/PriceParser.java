3c009a81253c6eba52da7d49745d46f437aee602
LittlePox <e0005280@u.nus.edu>
2018-05-31 23:34:00 +0800

    first working ver

--- src/main/java/com/littlepox/priceparser/PriceParser.java
+++ src/main/java/com/littlepox/priceparser/PriceParser.java
@@ -5,7 +5,19 @@ import java.util.*;
 import org.json.*;
 
 public class PriceParser {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException{
+        Scanner stdin = new Scanner(System.in);
+        int productId = stdin.nextInt();
+        stdin.nextLine();
+        String content = stdin.nextLine();
+        JSONObject obj = new JSONArray(content).getJSONObject(0);
+        JSONArray prices = obj.getJSONArray("price");
+        try (PrintWriter fout = new PrintWriter(productId + "_prices.csv")) {
+            for (int i = 0; i < prices.length(); i++) {
+                JSONObject entry = prices.getJSONObject(i);
+                fout.printf("%d,%s,%.6f%n", productId, entry.getString("date"), entry.getDouble("value"));
+            }
+            fout.close();
+        }
     }
 }
